f027ce363e64122ef3f7d18963fcab02a2f3f7fb
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2021-02-03 14:43:59 +0400

    Cool

---
+++ app/src/main/java/com/fragments/ChatFragment.kt
@@ -0,0 +1,8 @@
+package com.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.chatapp.R
+
+class ChatFragment : Fragment(R.layout.fragment_chat) {
+
+}
---
+++ app/src/main/java/com/fragments/LoginFragment.kt
@@ -0,0 +1,8 @@
+package com.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.chatapp.R
+
+class LoginFragment: Fragment(R.layout.fragment_login) {
+
+}
