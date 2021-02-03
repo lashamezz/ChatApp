984aa06b630e284009b4fb513c8d9a8d503c254b
mezzlasha <mezzlasha@gmail.com>
2021-02-03 20:35:17 +0400

    gneed

---
+++ app/src/main/java/com/fragments/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.chatapp.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+}
---
+++ app/src/main/java/com/fragments/RegisterFragment.kt
@@ -0,0 +1,8 @@
+package com.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.chatapp.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register) {
+
+}
